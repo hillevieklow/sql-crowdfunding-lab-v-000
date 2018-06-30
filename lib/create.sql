6b4fdd6e1bf06227b468468a708ca0f0e74a160b
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-30 19:44:29 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -5,5 +5,10 @@ CREATE TABLE projects (
   start_date TEXT,
   end_date TEXT
 );
-CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
 CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER)
